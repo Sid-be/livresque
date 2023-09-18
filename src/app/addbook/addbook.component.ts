e63336999ae90d103a40682a8db09ff520fc94d5
Sid.be <sid.be@outlook.com>
2023-09-18 13:10:35 +0200

    add component improvements

--- src/app/addbook/addbook.component.ts
+++ src/app/addbook/addbook.component.ts
@@ -1,11 +1,10 @@
-
 import { Component, OnInit } from '@angular/core';
 import { BooksService } from '../shared/books.service';
 import { Book } from '../livres';
 import { CrudService } from '../shared/crud.service';
 import { HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { of ,map,Subscription,skip} from 'rxjs';
+import { of, map, Subscription, skip } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import {
   FormControl,
@@ -14,7 +13,6 @@ import {
   Validators,
 } from '@angular/forms';
 import { HotToastService } from '@ngneat/hot-toast';
-
 
 @Component({
   selector: 'app-addbook',
@@ -29,156 +27,123 @@ export class AddbookComponent implements OnInit {
     private formBuilder: FormBuilder,
     private crudService: CrudService,
     private toast: HotToastService
-  ) {
-   
-   
-  }
-  form:FormGroup;
-  id:string='';
+  ) {}
+  form: FormGroup;
+  id: string = '';
   livre: Book;
   isbnID: string;
   starRating = 0;
   dateTime: Date;
   newLivre: any;
-  imageSrc: any;
+  imageSrc: any="./assets/No-Image-Placeholder.svg";
   btnStyle: string = 'hide';
   resume: any;
-  genresList :string[]= ['science-fiction', 'polar', 'fantaisie', 'Roman'];
+  genresList: string[] = ['science-fiction', 'polar', 'fantaisie', 'Roman'];
   isLoading: boolean = false;
- 
-  
- 
 
   ngOnInit(): void {
-     
-    this.actRoute.paramMap.subscribe(params => {
+    this.actRoute.paramMap.subscribe((params) => {
       this.isbnID = params.get('id');
-     
-      
     });
-   
-     
-   
   }
   isbnForm = new FormControl('', [
     Validators.required,
-    Validators.pattern('/^(?:ISBN(?:-1[03])?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$|97[89][0-9]{10}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)(?:97[89][- ]?)?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$/'),
+    Validators.pattern(
+      '/^(?:ISBN(?:-1[03])?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$|97[89][0-9]{10}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)(?:97[89][- ]?)?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$/'
+    ),
   ]);
-  title=new FormControl();
-  publisher=new FormControl();
-  authors= new FormControl();
-  publiDate=new FormControl();
+  title = new FormControl();
+  publisher = new FormControl();
+  authors = new FormControl();
+  publiDate = new FormControl();
   image = new FormControl('');
   synopsis = new FormControl();
   favoris = new FormControl();
-  
-  genres=new  FormControl();
-  
 
+  genres = new FormControl();
 
-
-  
-
-
-  
   getnewImage(): Observable<any> | any {
     if (this.livre != null) {
       return of(this.livre);
     }
   }
   onUpdate() {
-  /*   this.getnewImage().subscribe((data) => {
-      this.newLivre = data;
-     console.log(this.newLivre)
-    });
-    this.livre.image = this.image.value;
+    console.log(this.image.value);
+  /*   this.livre.image = this.image.value; */
    
     if(!this.imageSrc){
       this.imageSrc="/src/assets/img-placeholder.svg"
 
-    } */
-    https://m.media-amazon.com/images/I/511BXv5KYYL._SX307_BO1,204,203,200_.jpg
-   if(this.image && this.image.value){
-    this.imageSrc = this.image.value; // Update imageSrc with the new value
-    this.image.setValue('');
-  }
+    }
+    if (this.image && this.image.value) {
+      this.imageSrc = this.image.value; // Update imageSrc with the new value
+      this.image.setValue('');
+    }
   }
   getBook() {
-    console.log(this.isbnForm.value)
+    console.log(this.isbnForm.value);
     this.isLoading = true;
-   this.books.getData(this.isbnForm.value).subscribe(
-  ([cover, resumeData]) => {
-    console.log(cover)
-    
-  
+    this.books.getData(this.isbnForm.value).subscribe(
+      ([cover, resumeData]) => {
+        console.log(cover);
+
         this.isLoading = false;
-        this.imageSrc=cover;
-        if(resumeData.totalItems!==0){
+        this.imageSrc = cover;
+        if (resumeData.totalItems !== 0) {
           this.resume = resumeData.items[0]?.volumeInfo;
-          }
-   
-     },
-     (error) => {
-      console.error(error);
-      this.isLoading = false; // Fin du chargement (échec)
-    }
-     );
-    
-   
+        }
+      },
+      (error) => {
+        console.error(error);
+        this.isLoading = false; // Fin du chargement (échec)
+      }
+    );
   }
   submitBookData() {
-    const livre = new Book()
-  
-   if(this.resume){
-   
-  livre.title= this.resume? this.resume.title:this.title.value;
-  livre.isbn=this.isbnID;
-  livre.publisher=this.resume.publisher?this.resume.publisher:this.publisher.value;
-  livre.publishedDate=this.resume.publishedDate;
-  livre.image=this.imageSrc
-  livre.synopsis=this.resume?this.resume.description:this.synopsis.value
-   livre.authors=this.resume.authors;
-   livre.pages=this.resume.pageCount;
-   livre.langage=this.resume.language;
-   livre.favoris=this.favoris.value;
-   livre.genre=this.genres.value
-  /*  if(this.genre){
+    const livre = new Book();
+
+    if (this.resume) {
+      livre.title = this.resume ? this.resume.title : this.title.value;
+      livre.isbn = this.isbnID;
+      livre.publisher = this.resume.publisher
+        ? this.resume.publisher
+        : this.publisher.value;
+      livre.publishedDate = this.resume.publishedDate;
+      livre.image = this.imageSrc;
+      livre.synopsis = this.resume
+        ? this.resume.description
+        : this.synopsis.value;
+      livre.authors = this.resume.authors;
+      livre.pages = this.resume.pageCount;
+      livre.langage = this.resume.language;
+      livre.favoris = this.favoris.value;
+      livre.genre = this.genres.value;
+      /*  if(this.genre){
    console.log(this.genre)
    livre.genre=this.genre  
   } */
-  console.log(this.publisher.value)
-this.crudService.setUserBook(livre)
-    
-  
-this.crudService.notification$.pipe(skip(1)).subscribe((message) => {
- 
-  this.toast.info(message)
+      console.log(this.publisher.value);
+      this.crudService.setUserBook(livre);
 
-  })
-
-    
-   
+      this.crudService.notification$.pipe(skip(1)).subscribe((message) => {
+        this.toast.info(message);
+      });
+    } else {
+      livre.title = this.title.value;
+      livre.isbn = this.isbnID;
+      livre.publisher = this.publisher.value;
+      livre.publishedDate = this.publiDate.value;
+      livre.image = this.imageSrc;
+      livre.synopsis = this.synopsis.value;
+      livre.authors = this.authors.value;
+      livre.pages = 200;
+      livre.langage = 'fr';
+      livre.favoris = this.favoris.value;
+      livre.genre = this.genres.value;
+      console.log(livre);
+      this.crudService.setUserBook(livre);
+    }
   }
-  else{
-    livre.title= this.title.value;
-  livre.isbn=this.isbnID;
-  livre.publisher=this.publisher.value;
-  livre.publishedDate=this.publiDate.value;
-  livre.image=this.imageSrc
-  livre.synopsis=this.synopsis.value
-   livre.authors=this.authors.value;
-   livre.pages=200;
-   livre.langage='fr';
-   livre.favoris=this.favoris.value;
-   livre.genre=this.genres.value;
-  console.log(livre)
-this.crudService.setUserBook(livre)
-
-
-  
-}
-  }
-
 
   ShowInput() {
     this.btnStyle = 'show';
